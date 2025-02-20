b64f9e253e35ec8bacbbbf20ac002b104cfdf410
rootx90 <166911388+rootx90@users.noreply.github.com>
2025-02-20 14:15:53 +0200

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,11 @@
-<script src="/assets/js/main.js"></script>
+<footer class="site-footer">
+    <div class="footer-content">
+        <p>&copy; 2025 Hospital Management System. All rights reserved.</p>
+    </div>
+ 
+</footer>
+
+<!-- JavaScript -->
+<script src="../assets/js/main.js"></script>
 </body>
 </html>
-
